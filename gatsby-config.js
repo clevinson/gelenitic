dcebaceb7c08f8b43d8b6f41f33d26ea01c18be8
Cory Levinson <cjlevinson@gmail.com>
2019-01-28 16:23:56 -0500

    git add netlify config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-netlify-cms'
   ],
 }
