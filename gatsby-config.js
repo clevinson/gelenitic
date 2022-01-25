e25196c7cb9994eaba5abca218cf8c51e86b33b0
Sarah Scott <sarahoscott@gmail.com>
2022-01-25 12:49:08 -0800

    upgrade gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Ways Inner Pass',
+    title: `Ways Inner Pass`,
   },
   plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-remark",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'src',
+        name: "src",
         path: `${__dirname}/src/`,
       },
     },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-styled-components',
-    'gatsby-disable-404'
   ],
-}
+};
