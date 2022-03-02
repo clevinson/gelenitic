6841b2b2ea85907bd869f2063507613290619fb1
Sarah Scott <sarahoscott@gmail.com>
2022-03-01 19:42:37 -0800

    006

---
+++ src/pages/digital/wip-006.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import GlobalStyle from "../../components/global-style.js";
+
+function Codas(props) {
+  return (
+    <>
+      <GlobalStyle />
+      <div />
+    </>
+  );
+}
+
+export default Codas;
