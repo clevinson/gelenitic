d3e92a73aabedaa71d49b706b491c1d55e077d97
Sarah Scott <sarahoscott@gmail.com>
2022-03-01 21:16:09 -0800

    svgdotjs

--- src/pages/digital/wip-006.js
+++ src/pages/digital/wip-006.js
@@ -1,13 +1,57 @@
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import GlobalStyle from "../../components/global-style.js";
+import { SVG } from "@svgdotjs/svg.js";
+import "@svgdotjs/svg.filter.js";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  background-color: snow;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImage = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 function Codas(props) {
+  const mount = useRef(null);
+
+  useEffect(() => {
+    const draw = SVG().addTo(mount.current).size("100%", "100%");
+    const image = draw.image("/assets/WIP006/teahouse.jpeg");
+    image.viewbox(0, 0, 100, 100).size("100%", "100%");
+
+    // let blur;
+    let hueRotate;
+    image.filterWith((add) => {
+      // blur = add.gaussianBlur(0.5);
+      hueRotate = add.colorMatrix("hueRotate", 0);
+    });
+    hueRotate.animate(1000000).loop(true, true).attr("values", 360);
+
+    return () => {
+      draw.clear();
+      image.clear();
+
+      mount.current = null;
+    };
+  }, []);
+
   return (
-    <>
+    <Container>
       <GlobalStyle />
-      <div />
-    </>
+
+      <BgImage ref={mount} />
+    </Container>
   );
 }
 
